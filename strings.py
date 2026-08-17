aece70a5be2c5d16bf56a7981510800c76dfa84d
Sujan <xi.sujan13@gmail.com>
2026-08-17 18:31:57 +0545

    string is learned

---
+++ strings.py
@@ -0,0 +1,18 @@
+for x in "sujan":
+    print(x)
+
+text = "My name is Sujan "
+print("Sujan " in text)
+
+up = "sujan rajbanshi"
+print(up.upper())
+
+space = "   sujan rajbanshi   "
+print(space.strip())
+
+split = "Sujan is a good boy, He is handsome"
+print(split.split(":"))
+
+gpa = 3.95
+print(f"My gpa in class 12 is {gpa}")
+
