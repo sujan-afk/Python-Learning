9c0f4fd98222252a57fa18a5f5746464a0900742
Sujan <xi.sujan13@gmail.com>
2026-08-18 16:28:06 +0545

    learned dict

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+family = [
+    {"name": "Sujan Rajbanshi", "Age": 18, "country" : "Nepal"},
+    {"name": "Sudip Rajbanshi", "Age": 21, "country" : "South Korea"},
+    {"name": "Sabitra Rajbanshi", "Age": 46, "country" : "Nepal"},
+    {"name": "Chakai Rajbanshi", "Age": None, "country" : "Saudi Arabia"},
+]
+
+for pariwar in family:
+    print(pariwar["name"], pariwar["Age"], pariwar["country"], sep=",")
