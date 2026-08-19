f2a646b90287b2127d3de78c1a0b1063e020470c
Sujan <xi.sujan13@gmail.com>
2026-08-20 02:09:31 +0545

    new changes in main

--- calculator.py
+++ calculator.py
@@ -2,7 +2,10 @@ def main():
     x = int(input("Enter a number : "))
     print(f"square of {x} is {square(x)}")
 
+
 def square(num):
     return num * num
 
-main()
+
+if __name__ == "__main__":
+    main()
