eddb09e739e6796d60403122f7298469effc2b4a
Sujan <xi.sujan13@gmail.com>
2026-08-22 01:45:40 +0545

    dictreader in csv file use

--- students.py
+++ students.py
@@ -1,9 +1,9 @@
 import csv 
 students = []
 with open("students.csv") as file:
-    reader = csv.reader(file)
-    for name, home in reader:
-        students.append({"name": name, "home":home})
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"]})
 
 for student in sorted(students,key=lambda student: student["name"], reverse = False):
     print(f"{student['name']} is from {student['home']}")
