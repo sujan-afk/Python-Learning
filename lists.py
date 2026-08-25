b9a149e80c1d03fb83fa801eaa6fcff4857516fe
Sujan <xi.sujan13@gmail.com>
2026-08-25 15:18:27 +0545

    listing understanding of basics and core

---
+++ lists.py
@@ -0,0 +1,20 @@
+fruits = ["apple", "banana"]
+print(fruits)
+
+fruits.append("lemon")
+print(fruits)
+
+fruits.insert(2, "blueberry")
+print(fruits)
+
+# pop_out = fruits.pop()   #remove the last string
+# # print(pop_out)   
+# print(fruits)
+
+asc = fruits.sort()
+print(fruits)
+
+num = [1,2,3,4,5]
+squ = [i*i for i in num]
+print(num)
+print(squ)
