c5d023846246c5397111bedaf5efa2a64b84a714
Sujan <xi.sujan13@gmail.com>
2026-08-31 14:58:26 +0545

    new copy methods implemented

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,12 @@
+details = {"name": "Sujan Rajbanshi", "Age": 18, "country" : "Nepal"}
+
+#To copy the dictionary
+detail_copy = details.copy()
+#Or detail_copy = dict(details)
+
+detail_copy["email"] = "xi.sujan13@gmail.com"
+print(detail_copy)
+
 family = [
     {"name": "Sujan Rajbanshi", "Age": 18, "country" : "Nepal"},
     {"name": "Sudip Rajbanshi", "Age": 21, "country" : "South Korea"},
